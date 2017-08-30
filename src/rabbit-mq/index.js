161e007e8ddd9cad28a62a79acf9ce2f32db7ba0
pvasarhelyi <pvasarhelyi@emarsys.com>
2017-08-30 19:53:18 +0200

    refactor(client): remove unnecessary variables

--- src/rabbit-mq/index.js
+++ src/rabbit-mq/index.js
@@ -2,14 +2,11 @@
 
 const Pool = require('./pool');
 
-let channels = {};
-let assertedQueues = {};
-
 module.exports = {
   create: async (amqpConfig, queueName, connectionType) => {
     const rabbitMq = Pool.create(amqpConfig, connectionType).getClient(queueName);
     await rabbitMq.connect();
-    await rabbitMq.createChannel(channels, assertedQueues);
+    await rabbitMq.createChannel();
 
     return rabbitMq;
   }
