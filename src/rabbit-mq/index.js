fb6a1e17b76d8356602cedd93d4b6f55c96f35cd
morzzz007 <morzzz007@gmail.com>
2017-09-12 10:54:07 +0200

    Revert "refactor(client): remove unnecessary variables"
    This reverts commit 161e007e8ddd9cad28a62a79acf9ce2f32db7ba0.

--- src/rabbit-mq/index.js
+++ src/rabbit-mq/index.js
@@ -2,11 +2,14 @@
 
 const Pool = require('./pool');
 
+let channels = {};
+let assertedQueues = {};
+
 module.exports = {
   create: async (amqpConfig, queueName, connectionType) => {
     const rabbitMq = Pool.create(amqpConfig, connectionType).getClient(queueName);
     await rabbitMq.connect();
-    await rabbitMq.createChannel();
+    await rabbitMq.createChannel(channels, assertedQueues);
 
     return rabbitMq;
   }
